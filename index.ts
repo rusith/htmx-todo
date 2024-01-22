7dd4113a9e37e84b9a0b3017c7212ae21c448288
Rusith <rusith@mail.com>
2024-01-22 09:06:42 +0530

    handlebars -> hbs

--- index.ts
+++ index.ts
@@ -1,15 +1,12 @@
 import express from "express";
-import { engine, create } from "express-handlebars";
+import { engine } from "express-handlebars";
 import bodyParser from "body-parser";
 
 const app = express();
 
-const c = create({
-  helpers: {},
-});
 
-app.engine("handlebars", c.engine);
-app.set("view engine", "handlebars");
+app.engine("hbs", engine({ extname: '.hbs'}));
+app.set("view engine", "hbs");
 app.set("views", "./views");
 app.use(bodyParser.urlencoded());
 
@@ -33,6 +30,10 @@ app.get("/edit-todo/:id", (req, res) => {
   res.render("partials/components/todo-modal", { todo, layout: false });
 });
 
+app.get("/add-todo", (req, res) => {
+  res.render("partials/components/todo-modal", { layout: false });
+});
+
 app.post("/update-todo/:id", (req, res) => {
   const todo = todos.find(todo => todo.id === Number(req.params.id));
   if (todo) {
@@ -40,7 +41,7 @@ app.post("/update-todo/:id", (req, res) => {
   }
 
   res.render("partials/components/update-todo", {
-    todos,
+    todo,
     layout: false,
   });
 });
@@ -48,7 +49,17 @@ app.post("/update-todo/:id", (req, res) => {
 app.delete("/delete-todo/:id", (req, res) => {
   todos = todos.filter(todo => todo.id !== Number(req.params.id));
   res.status(200);
-  res.send('true');
+  res.send("true");
+});
+
+app.post("/create-todo", (req, res) => {
+  const last = todos.at(todos.length - 1);
+  todos.push({ id: (last?.id || 0) + 1, title: req.body.title });
+
+  res.render("partials/components/create-todo", {
+    todos,
+    layout: false,
+  });
 });
 
 app.listen(3000);
